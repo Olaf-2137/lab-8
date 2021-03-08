180f8bf63b1319c82ba231eb9bdf0095fb008033
Olaf-2137 <79452673+Olaf-2137@users.noreply.github.com>
2021-03-08 23:25:55 +0100

    Create Cat.java

---
+++ zadanie 3/Cat.java
@@ -0,0 +1,9 @@
+package sample;
+public class Cats extends Animals{
+    public void cats(){
+        System.out.println("Cats meaow");
+    }
+    public void dogs(){
+        System.out.println("It's not a dog!");
+    }
+}
