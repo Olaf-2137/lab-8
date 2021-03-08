03fea829a84998d8c7bc1627e830721c85b2d045
Olaf-2137 <79452673+Olaf-2137@users.noreply.github.com>
2021-03-08 23:09:43 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Main {
+    public static void main(String[] args) {
+        //classy
+        clasa1 class1 = new clasa1();
+        clasa2 class2 = new clasa2();
+        //printy
+        System.out.println(class1.message());
+        System.out.println(class2.message());
+
+    }
+
+}
