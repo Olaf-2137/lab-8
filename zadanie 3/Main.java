ef17b7badbee09c54bddca3893df377417ed484a
Olaf-2137 <79452673+Olaf-2137@users.noreply.github.com>
2021-03-08 23:22:38 +0100

    Create Main.java

---
+++ zadanie 3/Main.java
@@ -0,0 +1,12 @@
+package sample;
+public class Main {
+    public static void main(String[] args) {
+       Dog dog = new Dog();
+
+       dog.dogs();
+      
+       Cats cat = new Cats();
+
+       cat.cats();
+    }
+}
